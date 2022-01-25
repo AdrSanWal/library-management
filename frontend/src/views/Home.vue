70d7b41aae86e11656bb8a0d9a809959aaabb42f
AdrSanWal <wallace581@hotmail.com>
2022-01-25 13:02:50 +0100

    finder works

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -16,7 +16,18 @@
         </div>
 
         <div class="table">
-            <DropdownSearch :item="itemsSelected" :data="jsonResults"/>
+            <transition name="matches">
+                <div class="drpdwn" v-if="jsonResults">
+                    <div class="col-drp">
+                        <p>{{ itemsSelected }}</p>
+                    </div>
+                    <div class="col-drp">
+                        <ul class="list">
+                            <li v-for="x of jsonResults" :key="x.id">{{ x.field }}</li>
+                        </ul>
+                    </div>
+                </div>
+            </transition>
         </div>
     </div>
 </template>
@@ -42,21 +53,23 @@ export default {
         const data = reactive({
             itemsSelected: 'books',
             query: '',
-            jsonResults: [],
+            jsonResults: null,
         })
 
         const search = async () => {
-
             if (data.query === '') {
-                data.jsonResults = []
+                data.jsonResults = null
             } else {
                 const path = `${data.itemsSelected}/?q=${data.query}`
                 const resp = await useApi('GET', path)
                 const results = resp.jsonResponse.value
-                data.jsonResults = results.map(x => ({"id": x['id'], "field": x[itemsFields[data.itemsSelected]]}))
+                if (Object.keys(results).length === 0) {
+                    data.jsonResults = null
+                } else {
+                    data.jsonResults = results.map(x => ({"id": x['id'], "field": x[itemsFields[data.itemsSelected]]}))
+                }
             }
         }
-
         return { itemsFields, ...toRefs(data), search }
     }
 }
@@ -112,9 +125,48 @@ p.selected {
 
 /* Dropdowns ------------------------------------------------------ Dropdowns */
 
-/* .table {
-    display: flex;
-    justify-content: center;
-} */
+.table {
+    width: var(--width-fnd);
+}
+
+.drpdwn {
+    margin: 15px;
+    padding: 0;
+    list-style-type: none;
+    transform-origin: top;
+    transition: transform 0.5s ease-in-out;
+}
+
+
+
+.col-drp>p {
+    background-color: var(--color-nav);
+    color:whitesmoke;
+    padding: 10px;
+    font-size: larger;
+    font-weight: bold;
+
+}
+
+.col-drp>ul {
+    background-color: var(--color-hover);
+    list-style-type: none;
+    margin: 0;
+    padding: 0;
+}
+
+.list>li {
+    padding: 15px 10px;
+    font-size: large;
+}
+
+.list>li:hover {
+    background-color: rgb(201, 195, 195);
+    cursor: pointer;
+}
+
+.matches-leave-to {
+    transform: scaleY(0);
+}
 
 </style>
